7baae6086df9696d76195ea574f3f40b79565f30
evertonse <evertonse.junior@gmail.com>
2024-07-01 04:59:08 -0300

    Mon Jul  1 04:59:08 AM -03 2024

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -9,7 +9,7 @@ return {
   { -- Fuzzy Finder (files, lsp, etc)
     'nvim-telescope/telescope.nvim',
     event = 'VimEnter',
-    branch = '0.1.x',
+    tag = '0.1.5',
     lazy = false,
     dependencies = {
       'nvim-lua/plenary.nvim',
