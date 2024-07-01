a7a51dca457b20e88ff0703af8e85678c87fcd56
evertonse <evertonse.junior@gmail.com>
2024-07-01 16:01:39 -0300

    Mon Jul  1 04:01:39 PM -03 2024

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -105,9 +105,46 @@ require('lazy').setup({
   { 'numToStr/Comment.nvim', opts = {} },
   --{ 'nvim-tree/nvim-web-devicons' },
   { 'moll/vim-bbye', lazy = false },
+  -- lazy.nvim
+  {
+    'folke/noice.nvim',
+    event = 'VeryLazy',
+    opts = {
+      -- add any options here
+    },
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      'MunifTanjim/nui.nvim',
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      -- 'rcarriga/nvim-notify',
+    },
+    config = function()
+      require('noice').setup {
+        lsp = {
+          -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+          override = {
+            ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+            ['vim.lsp.util.stylize_markdown'] = true,
+            ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
+          },
+        },
+        -- you can enable a preset for easier configuration
+        presets = {
+          inc_rename = true,
+          bottom_search = true, -- use a classic bottom cmdline for search
+          command_palette = true, -- position the cmdline and popupmenu together
+          long_message_to_split = true, -- long messages will be sent to a split
+          lsp_doc_border = false, -- add a border to hover docs and signature help
+        },
+      }
+    end,
+  },
   {
     'filipdutescu/renamer.nvim',
     branch = 'master',
+    lazy = false,
     event = 'LspAttch',
     dependencies = { 'nvim-lua/plenary.nvim' },
   },
