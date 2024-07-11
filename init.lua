11581eab5d7a9fc79e96ed7bc959a33c40c11338
evertonse <evertonse.junior@gmail.com>
2024-07-11 17:50:56 -0300

    Thu Jul 11 05:50:56 PM -03 2024

--- init.lua
+++ init.lua
@@ -3,24 +3,27 @@
 --    [x oil.nvim i think ] find faster neotree for windows
 --    [- Could not doit ] configure noice cmdline to enable normal mode on it
 --    [x Working fine ] setup inc-rename to work with noice
---    [c] checkout tabby for neovim and other
+--    [x] checkout tabby for neovim and other
 --    [x ] config oil.nvim to test it's performance
 --    [x] configure mini.surround to surround word with quotes similar to le nvim.surround
 --    [x] check keymaps
 --    [x win32-yank-bin as the culprit]check why shit be slow these days when moving arround
---    [ done ] check 'c' mapping for mini plugin
---    [ x Some other time ] see about this ---@field public performance?  done cmp.PerformanceConfig
+--    [x done ] check 'c' mapping for mini plugin
+--    [x Some other time ] see about this ---@field public performance?  done cmp.PerformanceConfig
 --    [x seem the same I'll keep using mini.surround then I'll see about chaging]see if surround is better than mini.surround
 --    [x I'm using auto-session [ ] see the source code later ]:mksession
---    [ ] ui select on telescope on normal mode by typing a number
+--    [x] ui select on telescope on normal mode by typing a number
 --    [ ] Search curious about the gui aspect of this: https://github.com/ray-x/guihua.lua
 --    [x] Mini lsp, recording
---    [ ] Create a telescope options to select from yank history
+--    [x] Create a telescope options to select from yank history
 --    [ ] MORE PLUGINS https://github.com/rockerBOO/awesome-neovim#cursorline
---    [ ] Take a look at https://github.com/ggandor/leap.nvim
---    [ ] Take a look at  hop.nvim
+--    [x] Take a look at https://github.com/ggandor/leap.nvim
+--    [x] Take a look at hop.nvim
 --    [ ] Take a look at https://github-wiki-see.page/m/nvim-telescope/telescope.nvim/wiki/Extensions
 --    [ ] REFACTOR: Make all keymaps in keymaps, and require 'keymaps'.telescope for example in plugin site
+--    [ ] See about make named sessions and named tabs
+--    [ ] NvimTree bulk renamed/delete and
+--    [ ] NvimTree possibly undo (working with trash)
 
 -- [[ Setting globals utils functions before any plugin config function has any chance try to use a nil Global function ]]
 require 'utils'
