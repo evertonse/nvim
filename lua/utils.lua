179ed1b7a92707dafba21ea9bf52af8f3c91ebd0
evertonse <evertonse.junior@gmail.com>
2024-07-10 22:46:08 -0300

    Wed Jul 10 10:46:08 PM -03 2024

--- lua/utils.lua
+++ lua/utils.lua
@@ -335,7 +335,7 @@ vim.g.self = {
   session_plugin = session_opts[2], --NOTE: Better note Idk, bugs with Telescope sometimes
   mini_pick = true,
   notification_poll_rate = 40,
-  file_tree = file_tree_opts[OnWindows() and 1 or 1],
+  file_tree = file_tree_opts[OnWindows() and 1 or 2],
   -- BufferPaths = {}, -- XXX: SomeHow it does not user when i's on vim.g, too make problems no cap
 }
 
