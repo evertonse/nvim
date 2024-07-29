942f564e44e8b74e7a88ef75f566f7ed706050c9
evertonse <evertonse.junior@gmail.com>
2024-07-29 01:03:20 -0300

    Mon Jul 29 01:03:20 AM -03 2024

--- lua/plugins/bufjump.lua
+++ lua/plugins/bufjump.lua
@@ -21,16 +21,14 @@ return {
       print 'jump backward in same buffer'
       require('bufjump').backward_same_buf()
     end, opts)
-    vim.keymap.set('n', '<C-S-B>', function()
-      vim.fn.confirm 'jump backward in same buffer'
-      --require('bufjump').forward_same_buf()
-    end, opts)
 
-    -- IMPORTANT: this is mapped to "ctrl + shift + ."
+    -- IMPORTANT( solved, leaving the comment as history documentation ): this is mapped to "ctrl + shift + ."
     -- But, in alacritty, I'm send the escape code for "crtl + shift + ." when I press 'crtl + shift + i'
     -- { key = "I", mods = "Control|Shift", chars = "\u001b[43;5u" }, # crtl + shift + .
     -- stoled from: https://neovim.discourse.group/t/how-can-i-map-ctrl-shift-f5-ctrl-shift-b-ctrl-and-alt-enter/2133/2
-    vim.keymap.set('n', '<C-+>', function()
+    -- DONE: Fixed after reading https://gist.github.com/fnky/458719343aabd01cfb17a3a4f7296799
+    -- Any crtl + shift + <key> can be send as escape code "\u001b".."[" .. ascii_char_code_for_key .. ";5u"
+    vim.keymap.set('n', '<C-S-i>', function()
       print 'jump forward in same buffer'
       require('bufjump').forward_same_buf()
     end, opts)
