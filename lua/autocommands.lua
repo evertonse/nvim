d92ebda65691d11b5e974847b79535f30575b850
evertonse <evertonse.junior@gmail.com>
2025-04-22 10:18:33 -0300

    Tue Apr 22 10:18:33 AM -03 2025

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -1065,11 +1065,13 @@ vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufRead' }, {
 })
 
 vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufRead' }, {
-  pattern = { '*.asm' },
+  pattern = { '*.asm', '*.fasm', 'fasm', '*.disasm' },
   callback = function()
-    vim.bo.filetype = 'fasm'
-    vim.bo.commentstring = ';%s'
-    -- or vim.api.nvim_command('set commentstring=//%s')
+    vim.schedule(function()
+      vim.bo.filetype = 'fasm'
+      -- vim.bo.commentstring = ';%s'
+      vim.api.nvim_command 'set commentstring=;%s'
+    end)
   end,
 })
 
