6281a71e022d0618f4b0227a84b84064115a1909
evertonse <evertonse.junior@gmail.com>
2025-05-09 09:25:29 -0300

    Fri May  9 09:25:29 AM -03 2025

--- lua/plugins/lsp/lsp_saga.lua
+++ lua/plugins/lsp/lsp_saga.lua
@@ -1,8 +1,10 @@
+-- I don't think I'll ever need to use this, but theses configs in here are always like this XD
 require('lazy').setup {
   'nvimdev/lspsaga.nvim',
   config = function()
     require('lspsaga').setup {}
   end,
+  -- Docs: https://nvimdev.github.io/lspsaga/implement/
   dependencies = {
     'nvim-treesitter/nvim-treesitter', -- optional
     'nvim-tree/nvim-web-devicons', -- optional
